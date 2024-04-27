147a9ad3f356869fff46d4b1dcc80d746f379d8d
Kartikeye Pandey <53373648+Kartik242@users.noreply.github.com>
2024-04-27 13:55:47 +0530

    Add files via upload

---
+++ DI using Spring Boot/spring_boot_first/src/main/java/com/kartikeye/spring_boot_first/Alien.java
@@ -0,0 +1,12 @@
+package com.kartikeye.spring_boot_first;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+
+    public void code() {
+        System.out.println("Coding");
+    }
+
+}
