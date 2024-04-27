0b77f08fc73cbc848063a95b0a9609654f456d4b
Kartikeye Pandey <53373648+Kartik242@users.noreply.github.com>
2024-04-27 13:55:13 +0530

    Add files via upload

---
+++ First Spring Boot App/spring_boot_first/src/main/java/com/kartikeye/spring_boot_first/SpringBootFirstApplication.java
@@ -0,0 +1,14 @@
+package com.kartikeye.spring_boot_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstApplication.class, args);
+		System.out.println("This is the begining of the new journey towards java application");
+	}
+
+}
